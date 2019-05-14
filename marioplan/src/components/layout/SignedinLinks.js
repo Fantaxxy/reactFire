443ab2766d67dfb440c10922a9927ab1c4d3cd32
Jordan Byrd <jordan@baltimorehealthanalytics.com>
2019-05-14 16:41:09 -0400

    creating dashboard

---
+++ marioplan/src/components/layout/SignedinLinks.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+
+const SignedInLinks = () => {
+    return (
+       <ul className="right">
+        <li><NavLink to='/'>New Project</NavLink></li>
+        <li><NavLink to='/'>Log Out</NavLink></li>
+        <li><NavLink to='/' className='btn btn floating pink lighten-1'>JB</NavLink></li>
+       </ul>
+    )
+}
+
+export default SignedInLinks
---
+++ marioplan/src/components/projects/ProjectList.js
@@ -0,0 +1,38 @@
+// a functional component and pulls data from the dashbaord
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Jordan B.</p>
+                    <p className="grey-text">14th May, 4:420pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Jordan B.</p>
+                    <p className="grey-text">14th May, 4:420pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Jordan B.</p>
+                    <p className="grey-text">14th May, 4:420pm</p>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default ProjectList
